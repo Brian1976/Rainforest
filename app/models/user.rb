b5d0d46700c0e38f34302ee6e83638daaad25f3c
Brian1976 <brian.robinson1@yahoo.ca>
2013-07-21 22:26:55 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 # end 
 #- this is what was created but has "password_digest" which it says it shouldn't have until data gets saved to the model (maybe it is saved?)
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name
   has_secure_password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :name, :password, :on => :create
 end
